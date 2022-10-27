9f96d125d4901eaeeaf4c29aa4ae257171b68389
SMAM <57549163+SMAMHTN@users.noreply.github.com>
2022-10-27 12:29:05 +0700

    add another for mod 3

--- Modul3/Tugas/tgs_mod3/src/App.js
+++ Modul3/Tugas/tgs_mod3/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './Custom.scss';
+import dataAegis from './dataAegis';
+import { useState } from 'react';
+import SearchComponent from './components/search.component';
+import CardComponent from './components/card.component';
+import DateComponent from './components/date.component';
 
 function App() {
+  const [data, setData] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Praktikum PRLBK Kelompok 1</h1>
+      <DateComponent />
+      <SearchComponent dataAegis={dataAegis} setData={setData} />
+      <CardComponent filterData={data} />
+    </>
   );
 }
 
