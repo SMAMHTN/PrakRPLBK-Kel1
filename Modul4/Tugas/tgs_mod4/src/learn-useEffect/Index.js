c922d9abbb3e702966e8f94653d1d982a75f804d
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-11-02 22:23:19 +0700

    changes learn-useEffect and install react-router-bootstrap

--- Modul4/Tugas/tgs_mod4/src/learn-useEffect/Index.js
+++ Modul4/Tugas/tgs_mod4/src/learn-useEffect/Index.js
@@ -1,61 +1,53 @@
-import React, { useState, useEffect } from "react";
-import "./Index.css";
-export default function Index() {
-    const [count, setCount] = useState(0);
-    const [data, setData] = useState([]);
-    //dijalankan 1 kali
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setData(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-    //dijalankan terus setiap ada perubahan count
-    useEffect(() => {
-        if (count > 0) {
-            alert('component will update & count ${count}');
-        }
-    }, [count]);
-    //dijalankan terus menerus
-    useEffect(() => {
-        console.log('spam console kuy');
-    });
-    const countUp = () => {
-        setCount(count + 1);
-    };
-    const countDown = () => {
-        setCount(count - 1);
-    };
-    return (
-        <div className="Main">
-            <p className="Text"> Learn useEffect</p>
-            <p>KELOMPOK01</p>
-            <ul>
-                {data.slice(0, 10).map((value) => (
-                    <li key={value.id}>{value.title}</li>
-                ))}
-            </ul>
-            <p className="Text">{count}</p>
-            <div className="ViewButton">
+import React, { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/UserContext';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 
-                <div className="ViewButton2">
-                    <button className="Button"
-                        onClick={countUp}>
-                        Up
-                    </button>
-                </div>
-                <div className="ViewButton1">
-                    <button className="Button"
-                        onClick={countDown}>
-                        Down
-                    </button>
-                </div>
+export default function Effect() {
+  const [count, setCount] = useState(0);
+  const [isMount, setIsMount] = useState(false);
+  const { name } = useContext(UserContext);
+
+  // setIsMount to true when mounting
+  useEffect(() => {
+    setIsMount(true);
+  }, []);
+
+  // timer
+  useEffect(() => {
+    const interval =
+      isMount && setInterval(() => setCount((prev) => prev + 1), 1000);
+
+    return () => {
+      clearInterval(interval);
+      setCount(0);
+    };
+  }, [isMount]);
+
+  return (
+    <>
+      <Container>
+        <Stack gap={3}>
+          <h2 style={{ textAlign: 'center' }}>Welcome {name}</h2>
+          <Row>
+            <div style={{ textAlign: 'center' }}>
+              <h2>Timer</h2>
+              {isMount && <div>{count}</div>}
             </div>
+          </Row>
+          <Row></Row>
+        </Stack>
+        <div style={{ textAlign: 'center' }}>
+          <Button
+            variant='secondary'
+            onClick={() => setIsMount((prev) => !prev)}
+          >
+            Toggle mount
+          </Button>
         </div>
-    );
+      </Container>
+    </>
+  );
 }
