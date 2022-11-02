89e36a781efeb25127ecdea5683176f5fac9c563
haikalkilkul <ashidqi.haikal@gmail.com>
2022-11-02 22:08:50 +0700

    App.js + install react-bootstrap

--- Modul4/Tugas/tgs_mod4/src/App.js
+++ Modul4/Tugas/tgs_mod4/src/App.js
@@ -1,33 +1,53 @@
-// import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import "./App.css";
+import UserContext from "./context/UserContext";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Effect from "./learn-useEffect";
+import { useState } from "react";
+import InputComponent from "./learn-useState";
+import Context from "./learn-useContext";
+import AgeComponent from "./learn-useRef";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
-import Counter from './learn-useState/Counter';
-import Effect from './learn-useEffect/Index';
-import Context from './learn-useContext/Index';
 function App() {
+  const [name, setName] = useState("Strangers");
   return (
     <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Kelompok 1</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Use State</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/effect">
+                <Nav.Link>Use Effect</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/context">
+                <Nav.Link>Use Context</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/ref">
+                <Nav.Link>Use Ref</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className="App-header">
-        <nav className='NavBar'>
-          <ul className='ul'>
-            <li className='li'>
-              <Link to="/">Use State</Link>
-            </li>
-            <li className='li'>
-              <Link to="/effect">Use Effect</Link>
-            </li>
-            <li className='li'>
-              <Link to="/context">Use Context</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/' exact element={<Counter />} />
-          <Route path='/effect' exact element={<Effect />} />
-          <Route path='/context' exact element={<Context />} />
-        </Routes>
+        <UserContext.Provider value={{ name, setName }}>
+          {console.log(name)}
+          <Routes>
+            <Route path="/" exact element={<InputComponent />} />
+            <Route path="/effect" exact element={<Effect />} />
+            <Route path="/context" exact element={<Context />} />
+            <Route path="/ref" exact element={<AgeComponent />} />
+          </Routes>
+        </UserContext.Provider>
       </div>
     </Router>
   );
